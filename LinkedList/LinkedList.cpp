84b88e8a7102b056748cedec7f253081c50ba2ea
An Nguyen <nguyendinhan97@gmail.com>
2016-12-09 08:58:18 +0700

    8:58AM

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -17,7 +17,38 @@ void copyList(struct node *head1, int count1, struct node **head2, int *count2);
 
 int main()
 {
+	struct node *head1;
+	int count1;
+	init(&head1, &count1);
+	printf("%d",count1);
 	
+}
+
+void init(struct node **head, int *count)
+{
+	*head = NULL;
+	*count = 1;
+}
+
+void insert(struct node **head, int item, int index, int *count)
+{
+	if(index >= 0 && index<=(*count)){
+		struct node *newNode = (struct node *) malloc(sizeof(struct node));	
+		newNode->item = item;
+		newNode->next = NULL;
+		if(index == 0)
+		{
+			newNode->next = *head;
+			*head = newNode;	
+		}
+		else
+		{
+			struct node *cur = *head;
+			for(int i=0; i<pos; i++) cur = cur->next;
+			newNode->next = cur->next;
+			cur->next = newNode;
+		}
+	}
 }
 
 
