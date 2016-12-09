d3cafa798cefd1706c443a7f7773f075dbfc6e18
An Nguyen <nguyendinhan97@gmail.com>
2016-12-09 09:23:33 +0700

    Done 50%

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -1,16 +1,16 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 struct node
 {
 	int item;
 	struct node *next;
 };
 
-void init(struct node **head, int *count);
-void insert(struct node **head, int item, int index, int *count);
-void del(struct node **head, int index, int *count);
-bool isEmpty(struct node **head);
-void printList(struct node **head);
+void init(struct node **head, int *count);//
+void insert(struct node **head, int item, int index, int *count);//
+void del(struct node **head, int index, int *count);//
+bool isEmpty(struct node *head);//
+void printList(struct node *head);//
 void destroyList(struct node **head, int *count);
 void removeDuplicate(struct node **head, int *count);
 void copyList(struct node *head1, int count1, struct node **head2, int *count2);
@@ -20,14 +20,32 @@ int main()
 	struct node *head1;
 	int count1;
 	init(&head1, &count1);
-	printf("%d",count1);
+//	printf("%d\n", isEmpty(head1));
 	
+	for(int i=0; i<10; i++) insert(&head1, i, i, &count1);
+	printList(head1);
+	printf("\n");
+	
+	del(&head1, 0, &count1);
+	del(&head1, 0, &count1);
+	del(&head1, 0, &count1);
+	del(&head1, 0, &count1);
+	del(&head1, 0, &count1);
+	del(&head1, 0, &count1);
+	del(&head1, 0, &count1);
+	del(&head1, 0, &count1);
+	del(&head1, 0, &count1);
+	del(&head1, 0, &count1);
+	
+	printList(head1);
+	printf("%d\n", isEmpty(head1));
+
 }
 
 void init(struct node **head, int *count)
 {
 	*head = NULL;
-	*count = 1;
+	*count = 0;
 }
 
 void insert(struct node **head, int item, int index, int *count)
@@ -44,13 +62,49 @@ void insert(struct node **head, int item, int index, int *count)
 		else
 		{
 			struct node *cur = *head;
-			for(int i=0; i<pos; i++) cur = cur->next;
+			for(int i=0; i<index-1; i++) cur = cur->next;
 			newNode->next = cur->next;
 			cur->next = newNode;
 		}
+		(*count)++;
 	}
 }
 
+void del(struct node **head, int index, int *count)
+{
+	if(index>=0 && index<(*count))
+	{
+		struct node *temp = *head;
+		if(index == 0)
+		{
+			*head = temp->next;
+		}
+		else
+		{
+			struct node *cur = *head;
+			for(int i=0; i<index-1; i++) cur = cur->next;
+			temp = cur->next;
+			cur->next = temp->next;
+		}
+		free(temp);
+		(*count)--;
+	}
+}
+
+bool isEmpty(struct node *head)
+{
+	return head == NULL;
+}
+
+void printList(struct node *head)
+{
+	struct node *cur = head;
+	while(cur!=NULL)
+	{
+		printf("%d ", cur->item);
+		cur = cur->next;
+	}
+}
 
 
 
